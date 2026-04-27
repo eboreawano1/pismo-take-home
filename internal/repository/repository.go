24adb48f8374c7b1b2752fd3f12939ae7878d418
Ebore Awano <eboreawano1@gmail.com>
2026-04-26 21:32:52 -0500

    Add method to save events to the db

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"fmt"
+	"context"
 	"database/sql"
+	"pismo-take-home/internal/event"
 	_ "github.com/lib/pq"
 )
 
@@ -21,6 +23,38 @@ func New(databaseURL string) (*Repository, error) {
 	}
 
 	return &Repository{ database: database }, nil
+}
+
+func (repository *Repository) Save(ctx context.Context, event event.Event) error {
+	_, error := repository.database.ExecContext(ctx, `
+		INSERT INTO processed_events (
+			event_id,
+			event_type,
+			tenant_id,
+			status
+			producer,
+			payload,
+			event_time,
+			schema_version,
+		)
+		VALUES ($1, $2, $3, $4, $5, $6, $7::jsonb, $8)
+	`,
+		event.EventId,
+		event.EventType,
+		event.TenantId,
+		"READY_TO_DELIVER",
+		event.Producer,
+		event.Payload,
+		event.EventTime,
+		event.SchemaVersion,
+	
+	)
+
+	if error != nil {
+		return fmt.Errorf("Error while saving event: %w", error)
+	}
+		
+	return nil
 }
 
 func (repository *Repository) Close() error {
